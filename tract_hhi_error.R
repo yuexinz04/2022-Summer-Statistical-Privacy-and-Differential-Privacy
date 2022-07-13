3aaed6dcc1e49621cb31f826b33f498479760a56
leahghazali <108811638+leahghazali@users.noreply.github.com>
2022-07-13 14:41:25 -0400

    Update tract_hhi_error.R

--- tract_hhi_error.R
+++ tract_hhi_error.R
@@ -29,18 +29,20 @@ hhi_df <- tract_data %>%
           error = dp_total - sf_total)
 
 
+
+
 # al
 al_data = hhi_df[1:1181,]
 
 al_hhi_graph <- ggplot(al_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 al_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/al_hhi_graph.pdf", width=7.2, height=5.5)
@@ -50,14 +52,14 @@ ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/al_hhi_graph.pdf", width=7.2, he
 de_data = hhi_df[13700:13917,]
 
 de_hhi_graph <- ggplot(de_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 de_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/de_hhi_graph.pdf", width=7.2, height=5.5)
@@ -68,14 +70,14 @@ ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/de_hhi_graph.pdf", width=7.2, he
 la_data = hhi_df[28304:29451,]
 
 la_hhi_graph <- ggplot(la_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 la_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/la_hhi_graph.pdf", width=7.2, height=5.5)
@@ -86,14 +88,14 @@ ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/la_hhi_graph.pdf", width=7.2, he
 nc_data = hhi_df[48115:50309,]
 
 nc_hhi_graph <- ggplot(nc_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 nc_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/nc_hhi_graph.pdf", width=7.2, height=5.5)
@@ -104,14 +106,14 @@ ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/nc_hhi_graph.pdf", width=7.2, he
 pa_data = hhi_df[55347:58564,]
 
 pa_hhi_graph <- ggplot(pa_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 pa_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/pa_hhi_graph.pdf", width=7.2, height=5.5)
@@ -122,14 +124,14 @@ ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/pa_hhi_graph.pdf", width=7.2, he
 sc_data = hhi_df[58809:59911,]
 
 sc_hhi_graph <- ggplot(sc_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 sc_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/sc_hhi_graph.pdf", width=7.2, height=5.5)
@@ -140,14 +142,14 @@ ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/sc_hhi_graph.pdf", width=7.2, he
 ut_data = hhi_df[66896:67483,]
 
 ut_hhi_graph <- ggplot(ut_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 ut_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/ut_hhi_graph.pdf", width=7.2, height=5.5)
@@ -157,24 +159,22 @@ ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/ut_hhi_graph.pdf", width=7.2, he
 wa_data = hhi_df[69575:71032,]
 
 wa_hhi_graph <- ggplot(wa_data, aes(x = hhi, y = error, color = percent_old)) +
-  geom_point(alpha = .6) + labs(x = "HHI", y = "Error (people)", color="Population") +
+  geom_point(alpha = .4, size = .5, size = .5) + labs(x = "HHI", y = "Error (people)", color="Population") +
   geom_hline(yintercept=0, lty="dashed") + 
   scale_size_area(max_size=1.0, labels=comma, limits=c(0, 20e3), oob=squish) + 
   scale_color_viridis_c(option="A", labels = percent, begin = .3) +
   scale_x_continuous(labels=percent,expand=expansion(mult=0)) +
   scale_y_continuous(limits=c(-80, 80), expand=expansion(mult=0)) + 
   geom_line(stat="smooth", method = gam, formula = y~s(x, bs = "cs"), color = "#222222",
-            size = 1.5, se = FALSE, alpha = 0.5)
+            size = .65, se = FALSE, alpha = 0.5)
 
 wa_hhi_graph
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/wa_hhi_graph.pdf", width=7.2, height=5.5)
 
 
 
-hhi_figure <- ggarrange(al_hhi_graph, de_hhi_graph, la_hhi_graph, nc_hhi_graph, pa_hhi_graph,
-                         sc_hhi_graph, ut_hhi_graph, wa_hhi_graph,
+hhi_figure <- ggarrange(pa_hhi_graph, nc_hhi_graph, sc_hhi_graph, la_hhi_graph, al_hhi_graph,
+                         de_hhi_graph, ut_hhi_graph, wa_hhi_graph,
                          ncol = 4, nrow = 2, common.legend = TRUE, legend = "bottom")
 hhi_figure
 ggsave(".../nhgis_ppdd_20210428_12-2_tract/figs/hhi_figure.pdf", width = 9.54, height = 5.81)
-
-
